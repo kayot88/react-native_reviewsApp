24c72e174daa70f5efb5892e58f749ea595160df
Kayot88 <kayot88@gmail.com>
2020-01-08 14:55:17 +0200

    Header_backgroundImage

--- shared/header.js
+++ shared/header.js
@@ -1,24 +1,31 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
-const Header = ({navigation, title}) => {
+const Header = ({ navigation, title }) => {
   const openMenu = () => {
-    navigation.openDrawer()
+    navigation.openDrawer();
   };
 
   return (
-    <View style={styles.header}>
+    <ImageBackground
+      source={require("../assets/game_bg.png")}
+      style={styles.header}
+    >
       <MaterialIcons
         name="menu"
         onPress={openMenu}
         size={28}
         style={styles.icon}
       />
-      <View>
+      <View style={styles.headerTitle}>
+        <Image
+          style={styles.headerLogo}
+          source={require("../assets/heart_logo.png")}
+        />
         <Text style={styles.headerText}>{title}</Text>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
@@ -35,12 +42,22 @@ const styles = StyleSheet.create({
   headerText: {
     fontWeight: "bold",
     fontSize: 20,
-    color: "#fff",
-    letterSpacing: 1
+    color: "#333",
+    letterSpacing: 1,
+    marginHorizontal: 5
   },
   icon: {
     position: "absolute",
     left: 16
+  },
+  headerLogo: {
+    width: 20,
+    height: 20
+  },
+  headerTitle: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
 
